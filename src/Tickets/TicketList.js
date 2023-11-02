a3c5572661b34b331260ebae991b4343a662a6bf
Souradeep Kar <91742260+souradeep852@users.noreply.github.com>
2023-11-03 01:57:34 +0530

    Final commit
    Made a few changes in terms of the UI and the logos.

--- src/Tickets/TicketList.js
+++ src/Tickets/TicketList.js
@@ -1,22 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { fetchTickets, fetchUsers } from '../api';
-import { BiChevronDown, BiChevronUp,  } from 'react-icons/bi';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import { fetchTickets, fetchUsers } from "../api";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+import {
+  MdSignalCellularAlt,
+  MdSignalCellularAlt2Bar,
+  MdSignalCellularAlt1Bar,
+} from "react-icons/md";
+import { FcHighPriority } from "react-icons/fc";
+import { AiOutlineEllipsis } from "react-icons/ai";
+import "./styles.css";
 
+const iconMapping = {
+  0: <AiOutlineEllipsis />,
+  1: <FcHighPriority />,
+  2: <MdSignalCellularAlt />,
+  3: <MdSignalCellularAlt2Bar />,
+  4: <MdSignalCellularAlt1Bar />,
+};
+
+const priorityNameMapping = {
+  0: "No priority",
+  1: "Urgent",
+  2: "High",
+  3: "Medium",
+  4: "Low",
+};
 
 function TicketList() {
   const [tickets, setTickets] = useState([]);
   const [users, setUsers] = useState([]); // State to store user data
   const [error, setError] = useState(null);
   const [groupingOption, setGroupingOption] = useState(
-    localStorage.getItem('groupingOption') || 'status'
+    localStorage.getItem("groupingOption") || "status"
   );
   const [sortingOption, setSortingOption] = useState(
-    localStorage.getItem('sortingOption') || 'priority'
+    localStorage.getItem("sortingOption") || "priority"
   );
   const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-  
   useEffect(() => {
     // Fetch tickets data
     fetchTickets()
@@ -24,7 +45,7 @@ function TicketList() {
         setTickets(ticketsData);
       })
       .catch((error) => {
-        setError('Error fetching ticket data: ' + error);
+        setError("Error fetching ticket data: " + error);
       });
 
     // Fetch users data
@@ -34,39 +55,36 @@ function TicketList() {
         console.log(usersData);
       })
       .catch((error) => {
-        setError('Error fetching user data: ' + error);
+        setError("Error fetching user data: " + error);
       });
   }, []);
 
+  // Close the dropdown when clicking outside
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (isDropdownOpen && !e.target.closest(".dropdown-button")) {
+        setDropdownOpen(false);
+      }
+    };
 
+    document.addEventListener("click", handleOutsideClick);
 
-    // Close the dropdown when clicking outside
-    useEffect(() => {
-        const handleOutsideClick = (e) => {
-          if (isDropdownOpen && !e.target.closest('.dropdown-button')) {
-            setDropdownOpen(false);
-          }
-        };
-    
-        document.addEventListener('click', handleOutsideClick);
-    
-        return () => {
-          document.removeEventListener('click', handleOutsideClick);
-        };
-      }, [isDropdownOpen]);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isDropdownOpen]);
 
-      // Save grouping and sorting options to local storage
-    useEffect(() => {
-        localStorage.setItem('groupingOption', groupingOption);
-        localStorage.setItem('sortingOption', sortingOption);
-    }, [groupingOption, sortingOption]);
+  // Save grouping and sorting options to local storage
+  useEffect(() => {
+    localStorage.setItem("groupingOption", groupingOption);
+    localStorage.setItem("sortingOption", sortingOption);
+  }, [groupingOption, sortingOption]);
 
-
-//  // Use useEffect to log updated state values
-//  useEffect(() => {
-//     console.log('Grouping Option:', groupingOption);
-//     console.log('Sorting Option:', sortingOption);
-//   }, [groupingOption, sortingOption]);
+  //  // Use useEffect to log updated state values
+  //  useEffect(() => {
+  //     console.log('Grouping Option:', groupingOption);
+  //     console.log('Sorting Option:', sortingOption);
+  //   }, [groupingOption, sortingOption]);
 
   // Add functions to handle grouping and sorting options
 
@@ -85,25 +103,27 @@ function TicketList() {
   }
 
   // Grouping and sorting logic
-const groupedAndSortedTickets = () => {
+  const groupedAndSortedTickets = () => {
     let groupedTickets = {};
-  
+
     // Group tickets by the selected grouping option (e.g., by status or user)
     tickets.forEach((ticket) => {
       let groupKey;
       if (groupingOption === "user") {
-        groupKey = users.find((user) => user.id === ticket.userId)?.name || "Unknown User";
+        groupKey =
+          users.find((user) => user.id === ticket.userId)?.name ||
+          "Unknown User";
         console.log(groupKey);
       } else {
         groupKey = ticket[groupingOption];
       }
-  
+
       if (!groupedTickets[groupKey]) {
         groupedTickets[groupKey] = [];
       }
       groupedTickets[groupKey].push(ticket);
     });
-  
+
     // Sort the grouped tickets
     for (const groupKey in groupedTickets) {
       groupedTickets[groupKey] = groupedTickets[groupKey].sort((a, b) => {
@@ -114,67 +134,89 @@ const groupedAndSortedTickets = () => {
         }
       });
     }
-  
+
     return groupedTickets;
   };
-  
+
   const groupedAndSorted = groupedAndSortedTickets();
-  
 
   return (
-    <div className='main'>
-        <div className={`dropdown-button ${isDropdownOpen ? 'open' : ''}`}>
-            <button onClick={() => setDropdownOpen(!isDropdownOpen)}>
-            <span className="hamburger-icon">&#9776;</span> Display <span className="dropdown-icon">{isDropdownOpen ? <BiChevronUp /> : <BiChevronDown/>}</span>
-            </button>
-            {isDropdownOpen && (
-            <div className="dropdown">
-                <div className='group_by'>
-                    <label className='Grouping'>Grouping</label>
-                    <select className='select_Grouping' value={groupingOption} onChange={(e) => handleGroupingChange(e.target.value)}>
-                        <option value="status">Status</option>
-                        <option value="user">User</option>
-                        <option value="priority">Priority</option>
-                    </select>
-                </div>
-                <div className='order_by'>
-                <label className='Ordering'>Ordering</label>
-                <select className='select_Ordering' value={sortingOption} onChange={(e) => handleSortingChange(e.target.value)}>
-                    <option value="priority">Priority</option>
-                    <option value="title">Title</option>
-                </select>
-                </div>
+    <div className="main">
+      <div className={`dropdown-button ${isDropdownOpen ? "open" : ""}`}>
+        <button onClick={() => setDropdownOpen(!isDropdownOpen)}>
+          <span className="hamburger-icon">&#9776;</span> Display{" "}
+          <span className="dropdown-icon">
+            {isDropdownOpen ? <BiChevronUp /> : <BiChevronDown />}
+          </span>
+        </button>
+        {isDropdownOpen && (
+          <div className="dropdown">
+            <div className="group_by">
+              <label className="Grouping">Grouping</label>
+              <select
+                className="select_Grouping"
+                value={groupingOption}
+                onChange={(e) => handleGroupingChange(e.target.value)}
+              >
+                <option value="status">Status</option>
+                <option value="user">User</option>
+                <option value="priority">Priority</option>
+              </select>
             </div>
-            )}
-        </div>
+            <div className="order_by">
+              <label className="Ordering">Ordering</label>
+              <select
+                className="select_Ordering"
+                value={sortingOption}
+                onChange={(e) => handleSortingChange(e.target.value)}
+              >
+                <option value="priority">Priority</option>
+                <option value="title">Title</option>
+              </select>
+            </div>
+          </div>
+        )}
+      </div>
 
-        <div className="ticket-card-container">
-            {Object.keys(groupedAndSorted).map((groupKey) => (
-            <div key={groupKey}>
+      <div className="ticket-card-container">
+        {Object.keys(groupedAndSorted).map((groupKey) => (
+          <div key={groupKey}>
             <h2>
-              {groupingOption === 'user' && Array.isArray(users) && users.length > 0
-                ? groupKey
-                : groupingOption === 'priority'
-                ? `Priority: ${groupKey}`
-                : groupKey}
+              {groupingOption === "user" &&
+              Array.isArray(users) &&
+              users.length > 0 ? (
+                <div className="users-list-count">
+                  {groupKey}: {groupedAndSorted[groupKey].length}
+                </div>
+              ) : groupingOption === "priority" ? (
+                <div className="icon-priority">
+                  {iconMapping[groupKey]} {priorityNameMapping[groupKey]}:{" "}
+                  {groupedAndSorted[groupKey].length}
+                </div>
+              ) : (
+                <div className="users-list-count">
+                  {groupKey}: {groupedAndSorted[groupKey].length}
+                </div>
+              )}
             </h2>
 
-                <ul>
-                {groupedAndSorted[groupKey].map((ticket) => (
-                    <div key={ticket.id} className="ticket-card">
-                    <div className='card_ID'>{ticket.id}</div>
-                    <div className='title_ID'>{ticket.title}</div>
-                    <div className='feature_priority'>
-                        <div>Priority: {ticket.priority}</div>
-                        <div className='Feature_ID'>{ticket.tag.join(', ')}</div>
-                        </div>
-                    </div>
-                ))}
-                </ul>
-            </div>
-            ))}
-        </div>
-
+            <ul>
+              {groupedAndSorted[groupKey].map((ticket) => (
+                <div key={ticket.id} className="ticket-card">
+                  <div className="card_ID">{ticket.id}</div>
+                  <div className="title_ID">{ticket.title}</div>
+                  <div className="feature_priority">
+                    {groupingOption === "user" && (
+                      <div>{iconMapping[ticket.priority]}</div>
+                    )}
+                    <div className="Feature_ID">{ticket.tag.join(", ")}</div>
+                  </div>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
